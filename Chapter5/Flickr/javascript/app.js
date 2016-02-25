266daa4511d794ef6e76d5db5c5d192421b5a32b
SteveJanusz <sjanusz@bucs.fsw.edu>
2016-02-25 14:45:01 -0500

    Create Flickr app.

---
+++ Chapter5/Flickr/javascript/app.js
@@ -0,0 +1,9 @@
+var main = function() {
+	"use strict";
+	
+	//Flickr url.
+	var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
+	
+};
+
+$(document).ready(main);
