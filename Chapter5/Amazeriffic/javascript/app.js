d611e4be4fadcf6ebb979b85cccf876a8539a596
SteveJanusz <sjanusz@bucs.fsw.edu>
2016-02-25 16:01:40 -0500

    Chapter 5 Amazerrific

--- Chapter5/Amazeriffic/javascript/app.js
+++ Chapter5/Amazeriffic/javascript/app.js
@@ -6,11 +6,18 @@ var main = function(toDoObjects) {
 	});
 	
 	var addItemToList = function() {
-		var $input = $("main .content input");
-		var $val = $input.val();
-		if($val != "") {
-			toDos.push($val);
-			$input.val("");
+		var $description = $("main .content .description").val();
+		var $tags = $("main .content .tags").val().split(",");
+		
+		if($description != "" && $tags != "") {
+			toDoObjects.push({"description": $description, "tags": $tags})
+			
+			toDos = toDoObjects.map(function(toDo) {
+				return toDo.description;
+			});
+			
+			$("main .content .description").val("");
+			$("main .content .tags").val("");
 		}
 	};
 	
@@ -41,22 +48,56 @@ var main = function(toDoObjects) {
 			
 			} else if($element.parent().is(":nth-child(3)")) {
 				console.log("The tags tab was clicked.")
+				
+				var organizedByTag = organizeByTag(toDoObjects);
+				console.log(organizedByTag);
+				
+				organizedByTag.forEach(function(tag) {
+					var $tagName = $("<h3>").text(tag.name);
+					var $content = $("<ul>");
+						
+					tag.toDos.forEach(function(description) {
+						var $li = $("<li>").text(description);
+						$content.append($li);
+					});
+					
+					$("main .content").append($tagName);
+					$("main .content").append($content);
+				});
+				
 			} else if($element.parent().is(":nth-child(4)")) {
 
 				var $content = $("<div>");
 				
-				var $input = $("<input>");
+				var $inputLabel = $("<span>").text("Description: ");
+				var $input = $("<input>").addClass("description");
 				$input.on("keypress", function(event) {
 					if(event.keyCode === 13) {
 						addItemToList();
 					}
 				});
-				$content.append($input);
+				
+				var $tagLabel = $("<span>").text("Tags: ");
+				var $tagInput = $("<input>").addClass("tags");
+				$input.on("keypress", function(event) {
+					if(event.keyCode === 13) {
+						addItemToList();
+					}
+				});
 				
 				var $button = $("<button>").text("Add");
 				$button.on("click", function() {
 					addItemToList();
 				});
+				
+				$content.append($inputLabel);
+				$content.append($input);
+				$content.append($("<br>"));
+				
+				$content.append($tagLabel);
+				$content.append($tagInput);
+				$content.append($("<br>"));
+				
 				$content.append($button);
 				$("main .content").append($content);
 			
@@ -67,6 +108,30 @@ var main = function(toDoObjects) {
 	});
 	
 	$(".tabs a:first-child span").trigger("click");
+};
+
+var organizeByTag = function(toDoObjects) {
+	var tags = [];
+	
+	toDoObjects.forEach(function(toDo) {
+		toDo.tags.forEach(function(tag) {
+			if(tags.indexOf(tag) === -1) {
+				tags.push(tag);
+			}
+		});
+	});
+	
+	var tagObjects = tags.map(function(tag) {
+		var toDosWithTag = [];
+		toDoObjects.forEach(function(toDo) {
+			if(toDo.tags.indexOf(tag !== -10)) {
+				toDosWithTag.push(toDo.description);
+			}
+		});
+		
+		return {"name": tag, "toDos": toDosWithTag};
+	});
+	return tagObjects;
 };
 
 $(document).ready(function() {
