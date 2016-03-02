d9e8a55b140b8538b315ed122e6f759a5bc90963
SteveJanusz <sjanusz@bucs.fsw.edu>
2016-03-02 15:37:42 -0500

    Flickr slideshow implemented

--- Chapter5/Flickr/javascript/app.js
+++ Chapter5/Flickr/javascript/app.js
@@ -20,7 +20,38 @@ var searchForImages = function() {
 			//$img.fadeIn();
 			window.images.push(photo.media.m);
 		});
-	});	
+		
+		//Clear the current setTimeout so it is not running multiple times.
+		clearTimeout(window.timer);
+		displaySlideshow();
+	});
+	
+	
+}
+
+var displaySlideshow = function() {
+	
+	console.log("Test");
+	console.log(window.index);
+	console.log(window.images.length);
+	
+	//Remove previous image (if there is one)
+	$("main .photos").empty();
+	
+	//Display an image.
+	var $img = $("<img>").hide();
+	$img.attr("src", window.images[window.index]);
+	$("main .photos").append($img);
+	$img.fadeIn();
+	
+		
+	//Increment the index by 1, reset to 0 at the end.
+	window.index = (window.index+1) % window.images.length
+	
+	window.timer = setTimeout(function() {
+		//Run the function again.
+		displaySlideshow();
+	}, 3000);
 }
 
 $(document).ready(main);
