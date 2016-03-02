685795fa58fecc32810eadf61f6e0506becc2ec6
SteveJanusz <sjanusz@bucs.fsw.edu>
2016-03-02 14:43:14 -0500

    Flickr search button

--- Chapter5/Flickr/javascript/app.js
+++ Chapter5/Flickr/javascript/app.js
@@ -2,16 +2,26 @@ var main = function() {
 	"use strict";
 	
 	//Flickr url.
-	var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
+	$("main .input button").on("click", searchForImages);
+
+};
+
+var searchForImages = function() {
+	var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + $("main .input input").val() + "&format=json&jsoncallback=?";
+	
+	window.images = [];
+	window.index = 0;
 	
 	$.getJSON(url, function(flickrResponse) {
 		flickrResponse.items.forEach(function(photo) {
-			var $img = $("<img>").hide();
-			$img.attr("src", photo.media.m);
-			$("main .photos").append($img);
-			$img.fadeIn();
+			//var $img = $("<img>").hide();
+			//$img.attr("src", photo.media.m);
+			//$("main .photos").append($img);
+			//$img.fadeIn();
+			window.images.push(photo.media.m);
 		});
 	});
-};
+	
+}
 
 $(document).ready(main);
